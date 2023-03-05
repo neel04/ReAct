05e06d7fb7a8f71ce63a36be1f47469e34cdc19b
awesome <awesome@ip-172-31-44-155.ec2.internal>
2023-03-05 02:46:22 +0000

    Added Docker and enroot/pyxis

--- dpt_exec.sh
+++ dpt_exec.sh
@@ -2,11 +2,17 @@
 #SBATCH --job-name DPT_experiment   ## name that will show up in the queue
 #SBATCH --exclusive
 #SBATCH --output /fsx/awesome/dpt_log-%j.out   ## filename of the output; the %j is equal to jobID; default is slurm-[jobID].out
-#SBATCH --partition=g40423
+#SBATCH --partition=g40
 #SBATCH -t 5-24:00:00  ## time limit: (D-HH:MM)
 #SBATCH --account laion
 #SBATCH --ntasks-per-node=1
 #SBATCH --requeue ## requeue the job if preempted
+#------------------------------------------------------
+#SBATCH --container-image=neel04/react_image:latest 
+#SBATCH --container-mounts=/fsx/awesome:/fsx/awesome
+#SBATCH --container-workdir=/fsx/awesome/DPT
+#SBATCH --container-writable
+#SBATCH --container-name=react_image
 
 # Training on a slurm cluster
 # Personal WANDB API key
@@ -18,8 +24,7 @@ export PYTHONBREAKPOINT="web_pdb.set_trace"
 
 cd /fsx/awesome
 echo "Starting training" 
-singularity exec --nv -B /fsx/awesome:/fsx/awesome singularity_base_container.sif ipython /fsx/awesome/DPT/DeepThinking.ipynb
-echo "All Singularity Commands executed"
-# cp /fsx/awesome/dpt_log-${SLURM_JOB_ID}.out /fsx/awesome/DPT_MAIN-${SLURM_JOB_ID}.out
-# Explicitly exit the script
+#singularity exec --nv -B /fsx/awesome:/fsx/awesome singularity_base_container.sif ipython /fsx/awesome/DPT/DeepThinking.ipynb
+ipython /fsx/awesome/DPT/DeepThinking.ipynb
+echo "All Commands executed"
 exit 0
