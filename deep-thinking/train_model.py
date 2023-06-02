a08a4544c25dca0e600b1ddddf7f203e0e682dcc
Neel Gupta <neelgupta04@outlook.com>
2023-06-02 14:44:33 +0000

    Adam_on_lion | Seed=30847 | LR:1e-4

--- deep-thinking/train_model.py
+++ deep-thinking/train_model.py
@@ -220,7 +220,7 @@ def main(cfg: DictConfig):
 
 if __name__ == "__main__":
     # Setting the seed first for reproducibility
-    torch.manual_seed(69420)
+    torch.manual_seed(30847)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     
