16db204ad13d3ee6cee9ff42cc3910cabd35b719
Neel Gupta <neelgupta04@outlook.com>
2023-07-19 21:40:27 +0000

    Reverse binary list task

--- deep-thinking/train_model.py
+++ deep-thinking/train_model.py
@@ -68,7 +68,7 @@ def main(cfg: DictConfig):
 
     if accelerator.is_main_process:
         wandb.init(project="ReAct", id=run_id, config=dict(dic_cfg), 
-                magic=True, sync_tensorboard=False, group='Prefix_32')
+                magic=True, sync_tensorboard=False, group='Reversed_32')
         
         wandb.run.log_code("/fsx/awesome/DPT/", include_fn=lambda path: path.endswith(".py") or path.endswith(".ipynb") or path.endswith(".sh"))
 
