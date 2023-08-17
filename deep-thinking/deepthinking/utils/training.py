229208aa5f06a4c757df5601528959196d3fdada
Neel Gupta <neelgupta04@outlook.com>
2023-08-17 02:14:57 +0000

    re-enabling grads?

--- deep-thinking/deepthinking/utils/training.py
+++ deep-thinking/deepthinking/utils/training.py
@@ -54,6 +54,10 @@ def get_output_for_prog_loss(inputs, max_iters, net):
     perturbed_interim_thought, num_errors = corrupt_progress(interim_thought, output_head, epsilon=2e-4, steps=10) # add a few small errors
     perturbed_interim_thought = perturbed_interim_thought.detach()
 
+    # re-enable requires_grad=True for the parameters of out_head
+    for param in output_head.parameters():
+        param.requires_grad = True
+
     outputs, _ = net(inputs, iters_elapsed=n, iters_to_do=k, interim_thought=perturbed_interim_thought)
     return outputs, n+k, num_errors
 
