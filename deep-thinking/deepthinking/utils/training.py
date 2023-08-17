1954dd9ab77769a382c7d7817e066d9b6e175251
Neel Gupta <neelgupta04@outlook.com>
2023-08-17 01:25:50 +0000

    typo

--- deep-thinking/deepthinking/utils/training.py
+++ deep-thinking/deepthinking/utils/training.py
@@ -46,7 +46,9 @@ def get_output_for_prog_loss(inputs, max_iters, net):
     else:
         interim_thought = None
     
-    perturbed_interim_thought, num_errors = corrupt_progress(interim_thought, net.out_head, epsilon=2e-4, steps=10) # add a few small errors
+    perturbed_interim_thought, num_errors = corrupt_progress(interim_thought, net.module.out_head, epsilon=2e-4, steps=10) # add a few small errors
+    perturbed_interim_thought = perturbed_interim_thought.detach()
+
     outputs, _ = net(inputs, iters_elapsed=n, iters_to_do=k, interim_thought=perturbed_interim_thought)
     return outputs, n+k, num_errors
 
