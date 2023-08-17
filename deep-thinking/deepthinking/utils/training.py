bd45744d60c558f508779daf837a80a52873162e
Neel Gupta <neelgupta04@outlook.com>
2023-08-17 13:31:25 +0000

    epsilon = 2e-3

--- deep-thinking/deepthinking/utils/training.py
+++ deep-thinking/deepthinking/utils/training.py
@@ -53,7 +53,7 @@ def get_output_for_prog_loss(inputs, max_iters, net):
         param.requires_grad = False
 
     # Corrupt the thought tensor
-    interim_thought, num_errors = corrupt_progress(interim_thought, output_head, epsilon=2e-4, steps=10) # add a few small errors
+    interim_thought, num_errors = corrupt_progress(interim_thought, output_head, epsilon=2e-3, steps=10) # add a few small errors
     interim_thought = interim_thought.detach() if interim_thought is not None else interim_thought
 
     # re-enable requires_grad=True for the parameters of out_head
