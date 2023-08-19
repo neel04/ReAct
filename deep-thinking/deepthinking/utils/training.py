cec5db66045f5e8f56adcac8d27a74c81ca39fdc
Neel Gupta <neel@Neels-MacBook-Air.local>
2023-08-19 23:30:33 +0100

    Corrupting 4 bits

--- deep-thinking/deepthinking/utils/training.py
+++ deep-thinking/deepthinking/utils/training.py
@@ -80,9 +80,6 @@ def train(net, loaders, mode, train_setup, device, acc_obj=None):
 
     num_errors_dict = {i: num_errors.count(i) for i in range(0, max(num_errors) + 1)} # count the number of errors generated
 
-    if num_errors_dict[0] > sum(num_errors_dict[i] for i in range(1, max(num_errors) + 1)):
-        ProgressiveLossGenerator.learning_rate *= 1.5
-
     return loss, acc, train_mae, train_elem_acc, train_seq_acc, accelerator, num_errors
 
 def train_progressive(net, loaders, train_setup, device, accelerator=None):
